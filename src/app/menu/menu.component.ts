c656802e5c3c94babdb8f92b6c86703357844159
laupes <laura.pesce@jump-it.it>
2020-09-28 14:37:22 +0200

    creato menu per versione smartphone

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { $ } from 'protractor';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +12,13 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    $('.fa-times').hide();
 
+      $('.topnav').click(function() {
+        $('#myLinks').toggle();
+        $('.icon').toggle();
+        $('#logo').toggle();
+        $('.fa-times').toggle();
+      });
+  }
 }
