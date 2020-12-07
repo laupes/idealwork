48d3085a9b62b7ac90aa4331964b4977a2fb3edf
Laura Pesce <laurapesce@MacBook-Pro-di-Laura-2.local>
2020-12-06 21:25:15 -0800

    modificato menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,13 +12,25 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $('.fa-times').hide();
-
-      $('.topnav').click(function() {
-        $('#myLinks').toggle();
-        $('.icon').toggle();
-        $('#logo').toggle();
-        $('.fa-times').toggle();
-      });
+    var navbar = document.querySelector(".navbar")
+    var ham = document.querySelector(".ham")
+    
+    // toggles hamburger menu in and out when clicking on the hamburger
+    function toggleHamburger(){
+      navbar.classList.toggle("showNav")
+      ham.classList.toggle("showClose")
+    }
+    
+    ham.addEventListener("click", toggleHamburger)
+    
+    // toggle when clicking on links
+    
+    // METHOD 1
+    var menuLinks = document.querySelectorAll(".menuLink")
+    menuLinks.forEach( 
+      function(menuLink) { 
+        menuLink.addEventListener("click", toggleHamburger) 
+      }
+    )
   }
 }
