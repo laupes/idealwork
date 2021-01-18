da48c542bcbef9f15b235fdd9850c77eed7e4bd8
Laura Pesce <laurapesce@MacBook-Pro-di-Laura-2.local>
2021-01-18 05:14:31 -0800

    aggiunto carosello e sistemato responsive video

--- src/app/particolari-esecutivi/particolari-esecutivi-routing.module.ts
+++ src/app/particolari-esecutivi/particolari-esecutivi-routing.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CategoriaParticolariComponent } from './categoria-particolari/categoria-particolari.component';
+import { CaroselloComponent } from './carosello/carosello.component';
 
 const routes: Routes = [
     {
       path: 'particolari-esecutivi',
       component: CategoriaParticolariComponent,
-    }
+    },
+    {
+      path: 'preparazioni',
+      component: CaroselloComponent,
+    },
+    {
+      path: 'applicazioni',
+      component: CaroselloComponent,
+    },
+    {
+      path: 'protezioni',
+      component: CaroselloComponent,
+    },
 ];
 
 
