dd7866e67967514e74c52d2e4721f54c891633fb
Gennaro <gennaro.palazzo@jump-it.it>
2021-01-18 14:51:23 +0100

    carosello

--- src/app/particolari-esecutivi/carosello/carosello.component.ts
+++ src/app/particolari-esecutivi/carosello/carosello.component.ts
@@ -7,39 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaroselloComponent implements OnInit {
 
+  slideIndex = 1;
+
   constructor() { }
 
+  titolo: string;
+
   ngOnInit(): void {
+    this.titolo = sessionStorage.getItem('soluzione');
+    this.showSlides(this.slideIndex);
+  }
 
-    var slideIndex = 1;
-    showSlides(slideIndex);
+  // tslint:disable-next-line: typedef
 
-    // Next/previous controls
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
+
+  plusSlides(n: number): void {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  currentSlide(n: number): void {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number): void {
+    let i: number;
+    const slides = document.getElementsByClassName('mySlides') as HTMLCollectionOf<HTMLElement>;
+    const dots = document.getElementsByClassName('dot');
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
     }
-
-    // Thumbnail image controls
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
     }
-
-    function showSlides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";
-      dots[slideIndex-1].className += " active";
-    }
-    
+    slides[this.slideIndex - 1].style.display = 'block';
+    dots[this.slideIndex - 1].className += ' active';
   }
 
 }
